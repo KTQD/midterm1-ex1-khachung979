c9f9083ced17ec95309d967b030e2f96a605f4b2
khachung979 <157661539+khachung979@users.noreply.github.com>
2024-03-14 08:38:09 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("That Tinh", "A best ", "123", 110f, 280);
+        Book book = new Book("Dac Nhan Tam", "Sach hay", "366", 30.08f, "Author Name", 320, "Fiction");
 
-        MP3 mp3;
+        System.out.println("Information of MP3:");
         mp3.showInfo();
 
-        Book book;
+        System.out.println("\nInformation of Book:");
         book.showInfo();
-
     }
 }
